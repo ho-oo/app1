2213f7774089f73b2fd9131b80a3442e2977cf6e
ho-o <funnybarney@gmail.com>
2020-07-20 19:44:50 +0300

    portfolio

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Moikka, koodaus-bootcamp!");
+
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("Anna nimesi: ");
+            string nimi = Console.ReadLine();
+            Console.WriteLine("Hei!" + nimi + "!");
+
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+    }
+}
